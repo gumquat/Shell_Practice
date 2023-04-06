f0820e9a8af60c4a948cf0617f5765227f350b59
gumquat <gumquat@gmail.com>
2023-04-06 11:06:53 -0700

    :P

---
+++ forkWaitExec.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+int main() 
+{
+	int i, pid, status;
+
+	for (i = 0; i < 5; i++) 
+	{
+
+		pid = fork();
+
+		if (pid == 0) //child process 
+		{
+			execl("/bin/ls", "ls", "-l", "/tmp", (char*)NULL);
+			exit(0);
+		}
+		else if (pid > 0) //parent process
+		{
+			waitpid(pid, &status, 0);
+		}
+		else //error 
+		{
+			perror("couldn't fork");
+			exit(1);
+		}
+	}
+
+	return 0;
+}
+
